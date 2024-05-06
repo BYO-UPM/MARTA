c0040ffc3056979591bdf0f0c503ceefc4d7dfb2
aguerrerolopez <alexjorguer@tsc.uc3m.es>
2024-05-06 16:07:48 +0200

    running 3d models again

--- run_fold.sh
+++ run_fold.sh
@@ -13,29 +13,29 @@ python MARTA_Supervised.py --fold $FOLD --gpu 1 --latent_dim 3 --domain_adversar
 PID_GPU1=$!
 wait $PID_GPU0 $PID_GPU1
 
-echo "Marta classifier with latent dim 3 and domain adversarial"
-# Run the third pair of scripts simultaneously on GPU 0 and GPU 1
-python MARTA-S_classifier.py --fold $FOLD --gpu 0 --latent_dim 3 --domain_adversarial 1 &
-PID_GPU0=$!
-python MARTA-S_classifier.py --fold $FOLD --gpu 1 --latent_dim 3 --domain_adversarial 0 &
-PID_GPU1=$!
-wait $PID_GPU0 $PID_GPU1
+# echo "Marta classifier with latent dim 3 and domain adversarial"
+# # Run the third pair of scripts simultaneously on GPU 0 and GPU 1
+# python MARTA-S_classifier.py --fold $FOLD --gpu 0 --latent_dim 3 --domain_adversarial 1 &
+# PID_GPU0=$!
+# python MARTA-S_classifier.py --fold $FOLD --gpu 1 --latent_dim 3 --domain_adversarial 0 &
+# PID_GPU1=$!
+# wait $PID_GPU0 $PID_GPU1
 
-echo "Marta supervised with latent dim 32"
-# Run the second pair of scripts simultaneously on GPU 0 and GPU 1
-python MARTA_Supervised.py --fold $FOLD --gpu 0 --latent_dim 32 --domain_adversarial 1 &
-PID_GPU0=$!
-python MARTA_Supervised.py --fold $FOLD --gpu 1 --latent_dim 32 --domain_adversarial 0 &
-PID_GPU1=$!
-wait $PID_GPU0 $PID_GPU1
+# echo "Marta supervised with latent dim 32"
+# # Run the second pair of scripts simultaneously on GPU 0 and GPU 1
+# python MARTA_Supervised.py --fold $FOLD --gpu 0 --latent_dim 32 --domain_adversarial 1 &
+# PID_GPU0=$!
+# python MARTA_Supervised.py --fold $FOLD --gpu 1 --latent_dim 32 --domain_adversarial 0 &
+# PID_GPU1=$!
+# wait $PID_GPU0 $PID_GPU1
 
-echo "Marta classifier with latent dim 32 and domain adversarial"
-# Run the fourth pair of scripts simultaneously on GPU 0 and GPU 1
-python MARTA-S_classifier.py --fold $FOLD --gpu 0 --latent_dim 32 --domain_adversarial 1 &
-PID_GPU0=$!
-python MARTA-S_classifier.py --fold $FOLD --gpu 1 --latent_dim 32 --domain_adversarial 0 &
-PID_GPU1=$!
-wait $PID_GPU0 $PID_GPU1
+# echo "Marta classifier with latent dim 32 and domain adversarial"
+# # Run the fourth pair of scripts simultaneously on GPU 0 and GPU 1
+# python MARTA-S_classifier.py --fold $FOLD --gpu 0 --latent_dim 32 --domain_adversarial 1 &
+# PID_GPU0=$!
+# # python MARTA-S_classifier.py --fold $FOLD --gpu 1 --latent_dim 32 --domain_adversarial 0 &
+# # PID_GPU1=$!
+# wait $PID_GPU0 #$PID_GPU1
 
 echo "Finished scripts for fold $FOLD"
 done
