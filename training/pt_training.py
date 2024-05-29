Fix loss backpropagation for weightsum method, and remove reading best_threshold file

training/pt_training.py
@@ -694,13 +694,13 @@ def MARTA_trainer(
 
             optimizer.zero_grad()
 
-            if method == "weightsum": complete_loss.backward()
+            if method == "weightsum":
+                losses = [recon_loss, gaussian_loss, categorical_loss, 10*metric_loss]
 
             for i, loss_i in enumerate(losses):
 
-                if method != "weightsum":
-                    if i < NUM_LOSSES: loss_i.backward(retain_graph=True)
-                    else: loss_i.backward()
+                if i < NUM_LOSSES: loss_i.backward(retain_graph=True)
+                else: loss_i.backward()
 
                 grad2vec(model, grads, grad_dims, i)
                 model.zero_grad()
@@ -708,20 +708,18 @@ def MARTA_trainer(
             monitor_grad(e, batch_idx, grads)
             monitor_loss(e, batch_idx, losses)
             
-            if method != "weightsum":
-
-                if method == "cagrad":
-                    g = cagrad(grads)
-                elif method == "graddrop":
-                    g = graddrop(grads)
-                elif method == "mgd":
-                    g = mgd(grads)
-                elif method == "pcgrad":
-                    g = pcgrad(grads, rng)
-                elif  method == "sumloss":
-                    g = grads.sum(1)
+            if method == "cagrad":
+                g = cagrad(grads)
+            elif method == "graddrop":
+                g = graddrop(grads)
+            elif method == "mgd":
+                g = mgd(grads)
+            elif method == "pcgrad":
+                g = pcgrad(grads, rng)
+            else:
+                g = grads.sum(1)
             
-                overwrite_grad(model, g, grad_dims)
+            overwrite_grad(model, g, grad_dims)
 
             optimizer.step()
 
@@ -977,8 +975,7 @@ def MARTA_trainer(
                 },
                 name,
             )
-            sum_grad = grads.sum(1) if method != "weightsum" else \
-                       grads[:, 0] + grads[:, 1] + grads[:, 2] + 10*grads[:, 3]
+            sum_grad = grads.sum(1)
             sum_mag = torch.norm(sum_grad)
             with open(OUT_FILE_MAG, 'a') as f_mag:
                 f_mag.write(f'{e},{e},S,{sum_mag:.4f}\n')